a52595dc1e0e80464e180e33b211c6ccfc36cd9e
Dolan Reynolds <reynoldsdolan@gmail.com>
2024-04-30 01:28:15 -0400

    fix: positioning/scaling issue on homepage

--- src/app/components/Sections/Home.tsx
+++ src/app/components/Sections/Home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       className='h-[110vh] flex items-center justify-center bg-cover 2xl:bg-[url("/images/hh-4.jpg")] lg:bg-[url("/images/hh-11.jpg")] bg-[url("/images/hh-5.jpg")] lg:bg-center bg-[center_bottom_70%] overflow-hidden custom-clip-path-2'
       id='home'
     >
-      <div className='absolute left-1/2  -translate-x-1/2 -translate-y-1/2 lg:w-[calc(45vw+200px)] lg:top-[55%] w-[100vw] top-[90%] z-10'>
+      <div className='absolute flex justify-center items-center left-1/2 -translate-x-1/2 -translate-y-1/2 lg:w-[calc(45vw+200px)] lg:top-[55%] w-[100vw] top-[90%] z-10'>
         <motion.img
           src='/svg/hh-svg.svg'
           alt='Hush Hush Logo'
