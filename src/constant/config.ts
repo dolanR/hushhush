005dc143d822454bf23ef7b5b325e4368170f605
Dolan Reynolds <reynoldsdolan@gmail.com>
2024-04-30 00:49:23 -0400

    fix: small typo

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: 'HUSH HUSH | Official Website',
   description:
-    'The official website of the band Hush Hush. Music, videos, merch, and more.',
+    'The official website of the band HUSH HUSH. Music, videos, merch, and more.',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'https://hush-hush.live',
 };
