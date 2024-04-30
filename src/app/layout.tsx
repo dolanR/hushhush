970bdac4a6cdc1fc9c13cc04304b1e3fba159ea9
Dolan Reynolds <reynoldsdolan@gmail.com>
2024-04-30 00:11:56 -0400

    feat: added overscroll-none to html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html className='bg-neutral-900 scroll-smooth overflow-x-hidden'>
+    <html className='bg-neutral-900 scroll-smooth overflow-x-hidden overscroll-none'>
       <body>{children}</body>
     </html>
   );
