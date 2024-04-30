278d320e8dad3d26fe34f9232551142b33719d46
Dolan Reynolds <reynoldsdolan@gmail.com>
2024-04-30 01:25:24 -0400

    fix: metadata issue with og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     siteName: siteConfig.title,
-    images: [`${siteConfig.url}/images/og.jpg`],
+    // images: [`${siteConfig.url}/images/og.jpg`],
     type: 'website',
     locale: 'en_US',
   },
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: siteConfig.title,
     description: siteConfig.description,
-    images: [`${siteConfig.url}/images/og.jpg`],
+    // images: [`${siteConfig.url}/images/og.jpg`],
     // creator: '@th_clarence',
   },
   // authors: [
